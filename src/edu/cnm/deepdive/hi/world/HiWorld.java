3bee51257205bc34834909dcd718167d55b4612c
Vincent <vincentl.job@gmail.com>
2017-05-24 19:59:41 -0600

    # WARNING: head commit changed in the meantime
    test3

---
+++ src/edu/cnm/deepdive/hi/world/HiWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.hi.world;
+
+/**
+ * Simple class that displays the Hi world message
+ *
+ */
+public class HiWorld {
+	/**
+	 * Prints hi world
+	 * @param args Command-line parameters (ignored)
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi world!");
+		
+	}
+
+}
