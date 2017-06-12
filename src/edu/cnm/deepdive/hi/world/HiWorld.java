d93739b17a9ad404c1fdbfe468b840de6bd935a8
Vincent <vincentl.job@gmail.com>
2017-06-12 15:10:44 -0600

    h

--- src/edu/cnm/deepdive/hi/world/HiWorld.java
+++ src/edu/cnm/deepdive/hi/world/HiWorld.java
@@ -8,14 +8,27 @@ package edu.cnm.deepdive.hi.world;
  *
  */
 public class HiWorld {
-	/**
+	
+
+  /**
 	 * Prints hi world
 	 * @param args Command-line parameters (ignored)
+   * @param target 
 	 * 
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hi world!");
-		
+	public static void main(String[] args ) {
+	String target; String message;
+	if (args.length>0){
+	  target=args[0];
+	} else {
+	  target="World";
+	}
+	message=String.format("Hello %s!", target);
+	    emitMessage(message); 
+	
 	}
 
+	private static void emitMessage (String message) {
+	  System.out.printf(message); 
+	}
 }
